db9071e0d58d1a24d9fea77b292e95df00d810ac
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-03-21 00:45:00 -0300

    added default property done on toDo object, added list to do in localstorage on add a new item and developed logic to initialize and update the list to do through of lifecycle hooks manipulating the localstorage

--- scripts.js
+++ scripts.js
@@ -3,17 +3,30 @@ const toDoList = {
         return {
             appTitle: 'Lista de Tarefas',
             listToDo: [],
-            toDo: {}
+            toDo: {
+                done: false
+            }
         }
     },
     methods: {
         addToDo() {
             this.toDo.title && this.listToDo.push(this.toDo)
-            this.toDo = {}
+            this.toDo = {
+                done: false
+            }
+            localStorage.setItem('listToDo', JSON.stringify(this.listToDo))
         },
         clearToDo() {
             this.listToDo = []
         }
+    },
+    created() {
+        this.listToDo = localStorage.getItem('listToDo') 
+            ? JSON.parse(localStorage.getItem('listToDo')) 
+            : []
+    },
+    updated() {
+        localStorage.setItem('listToDo', JSON.stringify(this.listToDo))
     }
 }
 
